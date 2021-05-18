38b46c27c252b0bf9b0a6c04002998e7c5a49be2
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:41:13 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"There are a lot of flowers in the garden.  ___ are very beautiful.",
+        options:["It", "They", "We"],
+        answer: 1
+    },
+    {
+     q:"My friend and I are going to the party tonight.  ___ are exited.",
+        options:["We", "They", "You"],
+        answer: 0   
+    },
+    {
+        q:"Rachel is from USA.  ___ lives in New York.",
+        options:["It", "You", "She"],
+        answer: 2
+    },
+    {
+        q:"The cake is in the fridge.  ___ is very tasty. ",
+        options:[ "She", "He", "It"],
+        answer: 2
+    },
+    {
+        q:"You should read this book.  ___ is interesting.",
+        options:["It", "She", "They",],
+        answer: 0
+    },
+    {
+        q:"My son is 12 years old.  ___ is a very nice boy.",
+        options:["She", "He", "It"],
+        answer: 1
+    },
+    {
+        q:"The children are outside.  ___ are playing football.",
+        options:["They", "We", "You"],
+        answer: 0
+    },
+    {
+        q:"My cats like milk.  ___ drink milk every day.",
+        options:["It", "They", "He"],
+        answer: 1
+    },
+    {
+        q:"This parrot is nice. But ___ can't speak.",
+        options:["she", "it", "he"],
+        answer: 1
+    },
+    {
+        q:"Alex and Ann are students.  ___ study medicine at Edinburgh University.",
+        options:["We", "You", "They"],
+        answer: 2
+    }
+
+]
